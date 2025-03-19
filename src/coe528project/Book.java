a4a87665d9767dae23bb097b420995c9c2782984
Vallis-LFC <vkh.luong@gmail.com>
2025-03-19 17:49:10 -0400

    test

---
+++ src/coe528project/Book.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coe528project;
+import javafx.application.Application; 
+import javafx.scene.Scene; 
+import javafx.scene.control.*; 
+import javafx.scene.layout.*; 
+import javafx.event.ActionEvent; 
+import javafx.event.EventHandler; 
+import javafx.collections.*; 
+import javafx.stage.Stage; 
+/**
+ *
+ * @author vkhlu
+ */
+public class Book {
+    private String name;
+    private int price;
+    private CheckBox selected;
+    
+    public Book(String name, int price){
+        this.name = name;
+        this.price = price;
+        selected = new CheckBox("Book name: "+this.name+" Price: "+this.price);
+    }
+    
+    public void setSelected(boolean check){
+        (this.selected).setSelected(check);
+    }
+    
+    public boolean checkSelected(){
+        return this.selected.isSelected();
+    }
+}
