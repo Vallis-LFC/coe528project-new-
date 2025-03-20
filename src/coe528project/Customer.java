8e80a41e3f55074c566e8ae8ef9cdd8039c25212
Vallis-LFC <vkh.luong@gmail.com>
2025-03-19 23:55:50 -0400

    customer

---
+++ src/coe528project/Customer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coe528project;
+import javafx.application.Application; 
+import javafx.scene.Scene; 
+import javafx.scene.control.*; 
+import javafx.scene.layout.*; 
+import javafx.event.ActionEvent; 
+import javafx.event.EventHandler; 
+import javafx.collections.*; 
+import javafx.stage.Stage; 
+/**
+ *
+ * @author vkhlu
+ */
+public class Customer {
+    private int points;
+    private CheckBox select;
+    
+    
+}
