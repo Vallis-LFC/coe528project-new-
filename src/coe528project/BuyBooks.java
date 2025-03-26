4bc5816697fbd388c975043e1f19e11be1d8c590
Vallis-LFC <vkh.luong@gmail.com>
2025-03-26 18:02:55 -0400

    buybooks should be good?

--- src/coe528project/BuyBooks.java
+++ src/coe528project/BuyBooks.java
@@ -3,11 +3,52 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package coe528project;
-
+//import java.util.ArrayList;
 /**
  *
  * @author vkhlu
  */
 public class BuyBooks {
+    private static BuyBooks instance; //singleton to make the constructor unreachable so functions can be used without risk
+    private BuyBooks(){
+        
+        
+    }
     
+    public static BuyBooks getInstance(){
+        if(instance == null){
+            instance = new BuyBooks();
+        }
+        return instance;
+    }
+    
+    public int totalCost(Customer buyer){
+        int total = 0;
+        
+        for(int i = 0; i<buyer.getSavedBooks().size();i++){
+            total+=buyer.getSavedBooks().get(i).getPrice();
+        }
+        return total;
+    }
+    
+    public void buy(Library myLibrary, Customer buyer, int total){
+        buyer.addPoint(total);
+        for(int i = 0; i<buyer.getSavedBooks().size(); i++){
+            myLibrary.deleteBooks(buyer.getSavedBooks().get(i));
+        }
+        buyer.getSavedBooks().clear();
+        
+        
+    }
+    
+    public void redeemAndBuy(Library myLibrary, Customer buyer, int total){
+        total = buyer.minusPoint(total);
+        if (total>0){
+            buyer.addPoint(total);
+        }
+        for(int i = 0; i<buyer.getSavedBooks().size(); i++){
+            myLibrary.deleteBooks(buyer.getSavedBooks().get(i));
+        }
+        buyer.getSavedBooks().clear();
+    }
 }
