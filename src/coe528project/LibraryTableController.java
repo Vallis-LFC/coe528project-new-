c9d20c9824095d5802d17c3853410da5ffbbbaea
Monti_1 <muntasirmnr100@gmail.com>
2025-04-01 14:15:22 -0400

    ALWAYS CRASHING OUT

--- src/coe528project/LibraryTableController.java
+++ src/coe528project/LibraryTableController.java
@@ -6,6 +6,7 @@ package coe528project;
 
 import java.io.IOException;
 import javafx.collections.ObservableList;
+import javafx.collections.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,7 +14,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-
+import javafx.scene.control.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXMLLoader;
@@ -30,11 +31,13 @@ public class LibraryTableController implements Initializable {
 
     //Columns
     @FXML
-    private TableColumn<Book, String> nameColumn;
+    //private Label nameColumn;
+    private TableColumn<Book, String> nameColumn = new TableColumn<Book, String>();
 
 
     @FXML
-    private TableColumn<Book, Integer> priceColumn;
+    //private TableColumn<Book, Integer> priceColumn;
+    private TableColumn<Book, Integer> priceColumn = new TableColumn<Book, Integer>();
 
     //Text input
     @FXML
@@ -45,28 +48,41 @@ public class LibraryTableController implements Initializable {
 
 
 
-    private ObservableList<Book> myLibrary;
+    private ObservableList<Book> myLibrary = FXCollections.observableArrayList();
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         nameColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("name"));
         priceColumn.setCellValueFactory(new PropertyValueFactory<Book, Integer>("price"));
-        for(int i = 0; i<Library.getInstance().getLibrary().size();i++){
-            myLibrary.add(Library.getInstance().getLibrary().get(i));
+        if(this.myLibrary != null && !this.myLibrary.isEmpty() ){
+            for(int i = 0; i<Library.getInstance().getLibrary().size();i++){
+                myLibrary.add(Library.getInstance().getLibrary().get(i));
+                
+            }
         }
+	else{
+		System.out.println("null");
+          //makes a empty customer but since its not added into the list, doesnt matter
+          
+        }
+        
         tableView.setItems(myLibrary);
+        
     }
 
     //Submit button
     @FXML
     void addBook(ActionEvent event) {
         Book b = new Book(nameInput.getText(), Integer.parseInt(priceInput.getText()));
-        if (!Library.getInstance().verifyBooks(b)){
+        myLibrary = tableView.getItems();
+        
+        
+        if (Library.getInstance().verifyBooks(b)){
             myLibrary.add(b);
-            Library.getInstance().addBooks(b);
-        }
+            Library.getInstance().addBooks(b);}
         tableView.setItems(myLibrary);
+        
     }
 
     @FXML
@@ -80,7 +96,7 @@ public class LibraryTableController implements Initializable {
     
     @FXML
     void back(ActionEvent event) throws IOException{
-        Parent managerParent = FXMLLoader.load(getClass().getResource(""));   //change when u have the manager scene
+        Parent managerParent = FXMLLoader.load(getClass().getResource("OwnerView.fxml"));   //change when u have the manager scene
         Scene managerScene = new Scene(managerParent);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(managerScene);
